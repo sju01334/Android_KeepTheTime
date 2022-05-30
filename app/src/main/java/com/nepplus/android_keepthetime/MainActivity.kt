4c2e3ed6147a4800d5f0ef8615adc411c6601883
sju01334 <sju01334@gmail.com>
2022-05-30 09:41:21 +0900

    데이터 바인딩 및 기본 세팅완료

---
+++ app/src/main/java/com/nepplus/android_keepthetime/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.nepplus.android_keepthetime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.android_keepthetime.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
