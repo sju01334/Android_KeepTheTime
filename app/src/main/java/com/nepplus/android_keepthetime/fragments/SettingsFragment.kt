a8e8ec4c4e0ec1f0c7289f089516d50aadc15638
sju01334 <sju01334@gmail.com>
2022-05-30 15:11:07 +0900

    로그아웃 기능 구현

--- app/src/main/java/com/nepplus/android_keepthetime/fragments/SettingsFragment.kt
+++ app/src/main/java/com/nepplus/android_keepthetime/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.nepplus.android_keepthetime.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,10 @@ import androidx.databinding.DataBindingUtil
 import com.nepplus.android_keepthetime.R
 import com.nepplus.android_keepthetime.databinding.FragmentMyAppointmentsBinding
 import com.nepplus.android_keepthetime.databinding.FragmentSettingsBinding
+import com.nepplus.android_keepthetime.dialogs.CustomAlertDialog
+import com.nepplus.android_keepthetime.ui.main.LoginActivity
+import com.nepplus.android_keepthetime.utils.ContextUtil
+import com.nepplus.android_keepthetime.utils.GlobalData
 
 class SettingsFragment : BaseFragment() {
 
@@ -50,7 +55,28 @@ class SettingsFragment : BaseFragment() {
         binding.changePwLayout.setOnClickListener {  }
 //        로그아웃
         binding.logoutLayout.setOnClickListener {
+            val alert = CustomAlertDialog(mContext, requireActivity())
+            alert.myDialog()
 
+            alert.binding.titleTxt.text = "로그 아웃"
+            alert.binding.bodyTxt.text = "정말 로그아웃 하시겠습니까 ?"
+            alert.binding.contentEdt.visibility = View.GONE
+            alert.binding.positiveBtn.setOnClickListener {
+//                로그인 토큰(ContextUtil 에 있는) 만 제거하고 싶을 떄
+//                ContextUtil.setLoginToken(mContext, "")
+
+                ContextUtil.clear(mContext)
+                GlobalData.loginUser = null
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(myIntent)
+
+                alert.dialog.dismiss()
+
+            }
+            alert.binding.negativeBtn.setOnClickListener {
+                alert.dialog.dismiss()
+            }
         }
     }
 
