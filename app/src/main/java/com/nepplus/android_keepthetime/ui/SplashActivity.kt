9d91912836ec0331f67d8f8553f65c18e383dfc5
sju01334 <sju01334@gmail.com>
2022-05-30 10:05:32 +0900

    splashActivity 설정

---
+++ app/src/main/java/com/nepplus/android_keepthetime/ui/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.nepplus.android_keepthetime.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.nepplus.android_keepthetime.BaseActivity
+import com.nepplus.android_keepthetime.R
+import com.nepplus.android_keepthetime.ui.main.LoginActivity
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        var myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            val myIntent : Intent
+            myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+
+        },2500)
+    }
+}
---
+++ app/src/main/java/com/nepplus/android_keepthetime/ui/main/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.nepplus.android_keepthetime.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nepplus.android_keepthetime.BaseActivity
+import com.nepplus.android_keepthetime.R
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
